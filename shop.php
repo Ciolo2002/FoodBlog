02316c2020e9ab973f02a49e2d06ed899df560e0
Michael Sarto <michael.sarto@ptpvenezia.edu.it>
2021-04-28 20:17:17 +0200

    Images products

--- shop.php
+++ shop.php
@@ -19,13 +19,19 @@
         <header class="text-center logo">
             <h1 style="font-size: 65px">Products</h1>
         </header>
+        <?php
+        $stmt = $dbh->getInstance()->prepare("SELECT `IdProduct`,`Title`,`Link`,`Description`,images.Path FROM `products` INNER JOIN images on images.IdImage=products.IdImage WHERE 1");
+        $stmt->execute();
 
-        <div class="row">
+        for ($cnt = $stmt->rowCount(); $cnt > 0; --$cnt) {
+            $row = $stmt->fetch();
+            if (isset($row['IdProduct'])) {
+                echo '  <div class="row">
             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-45">
                 <div class="product-wrapper text-center">
                     <div class="product-img rounded">
-                        <a href="#" data-abc="true">
-                            <img src="https://i.imgur.com/tL7ZE46.jpg" class="rounded" alt="">
+                        <a href="' . $row['Link'] . '" data-abc="true">';
+                echo '<img src="' . $row['Path'] . '" class="rounded" alt="' . $row['Title'] . '">
                         </a>
                         
                         <div class="product-action">
@@ -35,47 +41,57 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>';
 
 
 
+                --$cnt;
+                $row = $stmt->fetch();
+            }
 
 
-
-
-            <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-45">
+            if (isset($row['IdProduct'])) {
+                echo '   <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-45">
                 <div class="product-wrapper  text-center">
                     <div class="product-img rounded">
-                        <a href="#" data-abc="true">
-                            <img src="https://i.imgur.com/lAQxXCK.jpg" class="rounded" alt="">
+                        <a href="' . $row['Link'] . '" data-abc="true">
+                            <img src="' . $row['Path'] . '" class="rounded" alt="' . $row['Title'] . '">
                         </a>
 
                         <div class="product-action">
                             <div class="product-action-style">
+                            <a href="#" class="nav-font">QUICK VIEW</a>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-45">
+            </div>';
+                --$cnt;
+                $row = $stmt->fetch();
+            }
+            if (isset($row['IdProduct'])) {
+                echo '   <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-45">
                 <div class="product-wrapper  text-center">
                     <div class="product-img rounded">
-                        <a href="#" data-abc="true">
-                            <img src="https://i.imgur.com/HxEEu5g.jpg" class="rounded" alt="">
+                        <a href="' . $row['Link'] . '" data-abc="true">
+                            <img src="' . $row['Path'] . '" class="rounded" alt="' . $row['Title'] . '">
                         </a>
 
                         <div class="product-action">
                             <div class="product-action-style">
+                            <a href="#" class="nav-font">QUICK VIEW</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
            
-        </div>
-
-
-
+        </div>';
+                --$cnt;
+                $row = $stmt->fetch();
+            }
+        }
+        ?>
 
 
 
