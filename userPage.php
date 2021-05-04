de58ccc6c9d46d58dd1e90b30f1db831bc1610d8
Michael Sarto <michael.sarto@ptpvenezia.edu.it>
2021-05-04 08:38:24 +0200

    i

--- userPage.php
+++ userPage.php
@@ -12,10 +12,8 @@
     <script src="registerPassowrdControl.js"> </script>
     <link rel=stylesheet href="userPage.css">
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script> -->
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
