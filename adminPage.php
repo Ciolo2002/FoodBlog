888e388a1a56d0b66915617e4486542f21937afc
Michael Sarto <michael.sarto@ptpvenezia.edu.it>
2021-05-06 22:40:10 +0200

    Table

--- adminPage.php
+++ adminPage.php
@@ -14,19 +14,27 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.9.1/extensions/export/bootstrap-table-export.js"></script>
     <script src="https://rawgit.com/hhurz/tableExport.jquery.plugin/master/tableExport.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.9.1/extensions/filter-control/bootstrap-table-filter-control.js"></script> -->
-<!-- https://codepen.io/AurelieT/pen/JGxMgo -->
+    <!-- https://codepen.io/AurelieT/pen/JGxMgo -->
     <link href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css" rel="stylesheet">
-
     <script src="https://unpkg.com/tableexport.jquery.plugin/tableExport.min.js"></script>
     <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.js"></script>
     <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table-locale-all.min.js"></script>
     <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/extensions/export/bootstrap-table-export.min.js"></script>
 
-
     <style rel="stylesheet" href="adminPage.css"></style>
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css">
+    <style>
+        .select,
+        #locale {
+            width: 100%;
+        }
 
-    
+        .operate {
+            margin-right: 10px;
+        }
+    </style>
+
+
 
 </head>
 
@@ -37,118 +45,232 @@
         <?php
         require_once("callingLogin.php");
         require_once("navbar.php"); ?>
-       
-        <div id="toolbar">
-            <select class="form-control">
-                <option value="">Export Basic</option>
-                <option value="all">Export All</option>
-                <option value="selected">Export Selected</option>
+
+
+
+
+
+        <div class="select">
+            <select class="form-control" id="locale">
+                <option value="af-ZA">af-ZA</option>
+                <option value="ar-SA">ar-SA</option>
+                <option value="ca-ES">ca-ES</option>
+                <option value="cs-CZ">cs-CZ</option>
+                <option value="da-DK">da-DK</option>
+                <option value="de-DE">de-DE</option>
+                <option value="el-GR">el-GR</option>
+                <option value="en-US" selected>en-US</option>
+                <option value="es-AR">es-AR</option>
+                <option value="es-CL">es-CL</option>
+                <option value="es-CR">es-CR</option>
+                <option value="es-ES">es-ES</option>
+                <option value="es-MX">es-MX</option>
+                <option value="es-NI">es-NI</option>
+                <option value="es-SP">es-SP</option>
+                <option value="et-EE">et-EE</option>
+                <option value="eu-EU">eu-EU</option>
+                <option value="fa-IR">fa-IR</option>
+                <option value="fi-FI">fi-FI</option>
+                <option value="fr-BE">fr-BE</option>
+                <option value="fr-FR">fr-FR</option>
+                <option value="he-IL">he-IL</option>
+                <option value="hr-HR">hr-HR</option>
+                <option value="hu-HU">hu-HU</option>
+                <option value="id-ID">id-ID</option>
+                <option value="it-IT">it-IT</option>
+                <option value="ja-JP">ja-JP</option>
+                <option value="ka-GE">ka-GE</option>
+                <option value="ko-KR">ko-KR</option>
+                <option value="ms-MY">ms-MY</option>
+                <option value="nb-NO">nb-NO</option>
+                <option value="nl-NL">nl-NL</option>
+                <option value="pl-PL">pl-PL</option>
+                <option value="pt-BR">pt-BR</option>
+                <option value="pt-PT">pt-PT</option>
+                <option value="ro-RO">ro-RO</option>
+                <option value="ru-RU">ru-RU</option>
+                <option value="sk-SK">sk-SK</option>
+                <option value="sv-SE">sv-SE</option>
+                <option value="th-TH">th-TH</option>
+                <option value="tr-TR">tr-TR</option>
+                <option value="uk-UA">uk-UA</option>
+                <option value="ur-PK">ur-PK</option>
+                <option value="uz-Latn-UZ">uz-Latn-UZ</option>
+                <option value="vi-VN">vi-VN</option>
+                <option value="zh-CN">zh-CN</option>
+                <option value="zh-TW">zh-TW</option>
             </select>
         </div>
 
-        <table id="table" data-toggle="table" data-search="true" data-filter-control="true" data-show-export="true" data-click-to-select="true" data-toolbar="#toolbar">
-            <thead>
-                <tr>
-                    <th data-field="state" data-checkbox="true"></th>
-                    <th data-field="Name" data-filter-control="input" data-sortable="true">Nane</th>
-                    <th data-field="Surname" data-filter-control="input" data-sortable="true">Surname</th>
-                    <th data-field="Email" data-filter-control="input" data-sortable="true">Email</th>
-                    <th data-field="Newsletter" data-filter-control="select" data-sortable="true">Newsletter</th>
-                    <!--  <th data-field="select" data-sortable="true">Note</th>  -->
-                    <th data-field="Category" data-filter-control="select" data-sortable="true">Category</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="0" name="btSelectItem" type="checkbox"></td>
-                    <td>Valérie</td>
-                    <td>01/09/2015</td>
-                    <td>Français</td>
-                    <td>12/20</td>
-                </tr>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="1" name="btSelectItem" type="checkbox"></td>
-                    <td>Eric</td>
-                    <td>05/09/2015</td>
-                    <td>Philosophie</td>
-                    <td>8/20</td>
-                </tr>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="2" name="btSelectItem" type="checkbox"></td>
-                    <td>Valentin</td>
-                    <td>05/09/2015</td>
-                    <td>Philosophie</td>
-                    <td>4/20</td>
-                </tr>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="3" name="btSelectItem" type="checkbox"></td>
-                    <td>Valérie</td>
-                    <td>05/09/2015</td>
-                    <td>Philosophie</td>
-                    <td>10/20</td>
-                </tr>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="4" name="btSelectItem" type="checkbox"></td>
-                    <td>Eric</td>
-                    <td>01/09/2015</td>
-                    <td>Français</td>
-                    <td>14/20</td>
-                </tr>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="5" name="btSelectItem" type="checkbox"></td>
-                    <td>Valérie</td>
-                    <td>07/09/2015</td>
-                    <td>Mathématiques</td>
-                    <td>19/20</td>
-                </tr>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="6" name="btSelectItem" type="checkbox"></td>
-                    <td>Valentin</td>
-                    <td>01/09/2015</td>
-                    <td>Français</td>
-                    <td>11/20</td>
-                </tr>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="7" name="btSelectItem" type="checkbox"></td>
-                    <td>Eric</td>
-                    <td>01/10/2015</td>
-                    <td>Philosophie</td>
-                    <td>8/20</td>
-                </tr>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="8" name="btSelectItem" type="checkbox"></td>
-                    <td>Valentin</td>
-                    <td>07/09/2015</td>
-                    <td>Mathématiques</td>
-                    <td>14/20</td>
-                </tr>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="9" name="btSelectItem" type="checkbox"></td>
-                    <td>Valérie</td>
-                    <td>01/10/2015</td>
-                    <td>Philosophie</td>
-                    <td>12/20</td>
-                </tr>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="10" name="btSelectItem" type="checkbox"></td>
-                    <td>Eric</td>
-                    <td>07/09/2015</td>
-                    <td>Mathématiques</td>
-                    <td>14/20</td>
-                </tr>
-                <tr>
-                    <td class="bs-checkbox "><input data-index="11" name="btSelectItem" type="checkbox"></td>
-                    <td>Valentin</td>
-                    <td>01/10/2015</td>
-                    <td>Philosophie</td>
-                    <td>10/20</td>
-                </tr>
-            </tbody>
+        <div id="toolbar">
+            <button id="remove" class="btn btn-danger" disabled>
+                <i class="fa fa-trash"></i> Delete
+            </button>
+        </div>
+        <table id="table" data-toolbar="#toolbar" data-search="true" data-show-refresh="true" data-show-toggle="true" data-show-fullscreen="true" data-show-columns="true" data-show-columns-toggle-all="true" data-detail-view="true" data-show-export="true" data-click-to-select="true" data-detail-formatter="detailFormatter" data-minimum-count-columns="2" data-show-pagination-switch="true" data-pagination="true" data-id-field="id" data-page-list="[10, 25, 50, 100, all]" data-show-footer="true" data-side-pagination="server" data-url="https://examples.wenzhixin.net.cn/examples/bootstrap_table/data" data-response-handler="responseHandler">
         </table>
 
+        <script>
+            var $table = $('#table')
+            var $remove = $('#remove')
+            var selections = []
 
+            function getIdSelections() {
+                return $.map($table.bootstrapTable('getSelections'), function(row) {
+                    return row.id
+                })
+            }
 
+            function responseHandler(res) {
+                $.each(res.rows, function(i, row) {
+                    row.state = $.inArray(row.id, selections) !== -1
+                })
+                return res
+            }
 
+            function detailFormatter(index, row) {
+                var html = []
+                $.each(row, function(key, value) {
+                    html.push('<p><b>' + key + ':</b> ' + value + '</p>')
+                })
+                return html.join('')
+            }
+
+            function operateFormatter(value, row, index) {
+                return [
+                    '<a class="operate" href="javascript:void(0)" title="Operate">',
+                    '<i class="fa fa-heart"></i>',
+                    '</a>  ',
+                    '<a class="remove" href="javascript:void(0)" title="Remove">',
+                    '<i class="fa fa-trash"></i>',
+                    '</a>'
+                ].join('')
+            }
+
+            window.operateEvents = {
+                'click .operate': function(e, value, row, index) {
+                    alert('You click operate action, row: ' + JSON.stringify(row))
+                },
+                'click .remove': function(e, value, row, index) {
+                    $table.bootstrapTable('remove', {
+                        field: 'id',
+                        values: [row.id]
+                    })
+                }
+            }
+
+            function totalTextFormatter(data) {
+                return 'Total'
+            }
+
+            function totalNameFormatter(data) {
+                return data.length
+            }
+
+            function totalPriceFormatter(data) {
+                var field = this.field
+                return '$' + data.map(function(row) {
+                    return +row[field].substring(1)
+                }).reduce(function(sum, i) {
+                    return sum + i
+                }, 0)
+            }
+
+            function initTable() {
+                $table.bootstrapTable('destroy').bootstrapTable({
+                    height: 550,
+                    locale: $('#locale').val(),
+                    columns: [
+                        [{
+                            field: 'state', //checobox per fare tutto
+                            checkbox: true,
+                            rowspan: 2,
+                            align: 'center',
+                            valign: 'middle'
+                        }, {
+                            title: 'User ID',
+                            field: 'id',
+                            rowspan: 2,
+                            align: 'center',
+                            valign: 'middle',
+                            sortable: true,
+                           
+                        }, {
+                            title: 'User Detail',
+                            colspan: 6,
+                            align: 'center'
+                        }],
+                        [{
+                            field: 'name',
+                            title: 'User Name',
+                            sortable: true,
+                           
+                            align: 'center'
+                        }, {
+                            field: 'surname',
+                            title: 'User Surname',
+                            sortable: true,
+                            align: 'center',
+                            
+                        }, 
+                        {
+                            field: 'email',
+                            title: 'User Email',
+                            sortable: true,
+                            align: 'center',
+                            
+                        },
+                        {
+                            field: 'newsletter',
+                            title: 'User Newsletter',
+                            checkbox: true,
+                            align: 'center',
+                            valign: 'middle'
+                        },
+                        {
+                            field: 'category',
+                            title: 'User Category',
+                            sortable: true,
+                            align: 'center',
+                            
+                        },{
+                            field: 'operate',
+                            title: 'Item Operate',
+                            align: 'center',
+                            clickToSelect: false,
+                            events: window.operateEvents,
+                           
+                        }]
+                    ]
+                })
+                $table.on('check.bs.table uncheck.bs.table ' +
+                    'check-all.bs.table uncheck-all.bs.table',
+                    function() {
+                        $remove.prop('disabled', !$table.bootstrapTable('getSelections').length)
+
+                        // save your data, here just save the current page
+                        selections = getIdSelections()
+                        // push or splice the selections if you want to save all data selections
+                    })
+                $table.on('all.bs.table', function(e, name, args) {
+                    console.log(name, args)
+                })
+                $remove.click(function() {
+                    var ids = getIdSelections()
+                    $table.bootstrapTable('remove', {
+                        field: 'id',
+                        values: ids
+                    })
+                    $remove.prop('disabled', true)
+                })
+            }
+
+            $(function() {
+                initTable()
+
+                $('#locale').change(initTable)
+            })
+        </script>
     </div>
 
 
