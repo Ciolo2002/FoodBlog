966dbc68ce245fb5bb21c1171309ee0fc75b1a25
Michael Sarto <michael.sarto@ptpvenezia.edu.it>
2021-05-07 19:35:26 +0200

    FESSSTAAAAAA LA TABELLAAAAA FUNGHEEEEEEEEEE

--- adminPage.php
+++ adminPage.php
@@ -4,71 +4,22 @@
 <head>
     <?php
     require_once("header.php");
-    $stmt = $dbh->getInstance()->prepare("SELECT `IdUser`, `Name`, `Surname`, `Email`,  `Newsletter`, categories.Category FROM `users` INNER join categories on categories.IdCategory=users.Category WHERE 1 ORDER by users.IdUser");
-    $stmt->execute();
-    while ($row = $stmt->fetch()) {
-        $users[] = array(
-            "iduser" => $row["IdUser"],
-            "name" => $row["Name"],
-            "surname" => $row["Surname"],
-            "email" => $row["Email"],
-            "newsletter" => $row["Newsletter"],
-            "category" => $row["Category"],
-        );
-    }
-
-    $file = "json/users.json";
-    if (file_put_contents(
-        $file,
-        json_encode($users)
-    ))
-        echo ("");
-    else
-        echo ("Failed");
     ?>
 
-    <script src="adminPage.js"></script>
-
-    
- 
-    
-    
-    
-    
-
-    <link href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css" rel="stylesheet">
-    <script src="https://unpkg.com/tableexport.jquery.plugin/tableExport.min.js"></script>
-    <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.js"></script>
-    <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table-locale-all.min.js"></script>
-    <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/extensions/export/bootstrap-table-export.min.js"></script>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="https://unpkg.com/bootstrap-table@1.16.0/dist/bootstrap-table.min.js"></script>
-
-    <style rel="stylesheet" href="adminPage.css"></style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css" />
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css">
-    
-    <style>
-        .select,
-        #locale {
-            width: 100%;
-        }
-
-        .operate {
-            margin-right: 10px;
-        }
-    </style>
-
-
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.1.1/jq-3.3.1/jszip-2.5.0/dt-1.10.24/b-1.7.0/b-colvis-1.7.0/b-html5-1.7.0/b-print-1.7.0/cr-1.5.3/fh-3.1.8/kt-2.6.1/r-2.2.7/datatables.min.css" />
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.1.1/jq-3.3.1/jszip-2.5.0/dt-1.10.24/b-1.7.0/b-colvis-1.7.0/b-html5-1.7.0/b-print-1.7.0/cr-1.5.3/fh-3.1.8/kt-2.6.1/r-2.2.7/datatables.min.js"></script>
 
 </head>
 
 <body>
-<link href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css" rel="stylesheet">
 
-<script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.js"></script>
     <div class="container-fluid ">
 
         <?php
@@ -76,258 +27,71 @@
         require_once("navbar.php"); ?>
 
 
-
-
-
-        <div class="select">
-            <select class="form-control" id="locale">
-                <option value="af-ZA">af-ZA</option>
-                <option value="ar-SA">ar-SA</option>
-                <option value="ca-ES">ca-ES</option>
-                <option value="cs-CZ">cs-CZ</option>
-                <option value="da-DK">da-DK</option>
-                <option value="de-DE">de-DE</option>
-                <option value="el-GR">el-GR</option>
-                <option value="en-US" selected>en-US</option>
-                <option value="es-AR">es-AR</option>
-                <option value="es-CL">es-CL</option>
-                <option value="es-CR">es-CR</option>
-                <option value="es-ES">es-ES</option>
-                <option value="es-MX">es-MX</option>
-                <option value="es-NI">es-NI</option>
-                <option value="es-SP">es-SP</option>
-                <option value="et-EE">et-EE</option>
-                <option value="eu-EU">eu-EU</option>
-                <option value="fa-IR">fa-IR</option>
-                <option value="fi-FI">fi-FI</option>
-                <option value="fr-BE">fr-BE</option>
-                <option value="fr-FR">fr-FR</option>
-                <option value="he-IL">he-IL</option>
-                <option value="hr-HR">hr-HR</option>
-                <option value="hu-HU">hu-HU</option>
-                <option value="id-ID">id-ID</option>
-                <option value="it-IT">it-IT</option>
-                <option value="ja-JP">ja-JP</option>
-                <option value="ka-GE">ka-GE</option>
-                <option value="ko-KR">ko-KR</option>
-                <option value="ms-MY">ms-MY</option>
-                <option value="nb-NO">nb-NO</option>
-                <option value="nl-NL">nl-NL</option>
-                <option value="pl-PL">pl-PL</option>
-                <option value="pt-BR">pt-BR</option>
-                <option value="pt-PT">pt-PT</option>
-                <option value="ro-RO">ro-RO</option>
-                <option value="ru-RU">ru-RU</option>
-                <option value="sk-SK">sk-SK</option>
-                <option value="sv-SE">sv-SE</option>
-                <option value="th-TH">th-TH</option>
-                <option value="tr-TR">tr-TR</option>
-                <option value="uk-UA">uk-UA</option>
-                <option value="ur-PK">ur-PK</option>
-                <option value="uz-Latn-UZ">uz-Latn-UZ</option>
-                <option value="vi-VN">vi-VN</option>
-                <option value="zh-CN">zh-CN</option>
-                <option value="zh-TW">zh-TW</option>
-            </select>
+        <div class="table-responsive">
+            <table id="users_data" class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <td>IdUser</td>
+                        <td>Name</td>
+                        <td>Surname</td>
+                        <td>Email</td>
+                        <td>Newsletter</td>
+                        <td>Category</td>
+                        <td>Operations</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $stmt = $dbh->getInstance()->prepare("SELECT `IdUser`, `Name`, `Surname`, `Email`,  `Newsletter`, categories.Category FROM `users` INNER join categories on categories.IdCategory=users.Category WHERE 1 ORDER by users.IdUser");
+                    $stmt->execute();
+                    while ($row = $stmt->fetch()) {
+                        echo '  
+                               <tr>  
+                                    <td>' . $row["IdUser"] . '</td>  
+                                    <td>' . $row["Name"] . '</td>  
+                                    <td>' . $row["Surname"] . '</td>  
+                                    <td>' . $row["Email"] . '</td>  
+                                    <td>' . $row["Newsletter"] . '</td>
+                                    <td>' . $row["Category"] . '</td>
+                                    <td>' . $row["Category"] . '</td>
+                                      
+                               </tr>  
+                               ';
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
 
-        <div id="toolbar">
-            <button id="remove" class="btn btn-danger" disabled>
-                <i class="fa fa-trash"></i> Delete
-            </button>
-        </div>
-        <table  id="table"
-  data-toolbar="#toolbar"
-  data-search="true"
-  data-show-refresh="true"
-  data-show-toggle="true"
-  data-show-fullscreen="true"
-  data-show-columns="true"
-  data-show-columns-toggle-all="true"
-  data-detail-view="true"
-  data-show-export="true"
-  data-click-to-select="true"
-  data-detail-formatter="detailFormatter"
-  data-minimum-count-columns="2"
-  data-show-pagination-switch="true"
-  data-pagination="true"
-  data-id-field="id"
-  data-page-list="[10, 25, 50, 100, all]"
-  data-show-footer="true"
-  data-side-pagination="server" 
-  data-url="json\users.json" 
-  data-response-handler="responseHandler">
-           
-        </table>
-
-
-        <script>
-            var $table = $('#table')
-            var $remove = $('#remove')
 
 
 
-            function getIdSelections() {
-                return $.map($table.bootstrapTable('getSelections'), function(row) {
-                    return row.id
-                })
-            }
 
-            function responseHandler(res) {
-                $.each(res.rows, function(i, row) {
-                    row.state = $.inArray(row.id, selections) !== -1
-                })
-                return res
-            }
 
-            function detailFormatter(index, row) {
-                var html = []
-                $.each(row, function(key, value) {
-                    html.push('<p><b>' + key + ':</b> ' + value + '</p>')
-                })
-                return html.join('')
-            }
 
-            function operateFormatter(value, row, index) {
-                return [
-                    '<a class="operate" href="javascript:void(0)" title="Operate">',
-                    '<i class="fas fa-cog"></i>',
-                    '</a>  ',
-                    '<a class="remove" href="javascript:void(0)" title="Remove">',
-                    '<i class="fa fa-trash"></i>',
-                    '</a>'
-                ].join('')
-            }
 
-            window.operateEvents = {
-                'click .operate': function(e, value, row, index) {
-                    alert('You click operate action, row: ' + JSON.stringify(row))
-                },
-                'click .remove': function(e, value, row, index) {
-                    $table.bootstrapTable('remove', {
-                        field: 'id',
-                        values: [row.id]
-                    })
-                }
-            }
 
-            
-
-            
-
-           
-
-            function initTable() {
-                $table.bootstrapTable('destroy').bootstrapTable({
-                    height: 550,
-                    locale: $('#locale').val(),
-                    columns: [
-                        [{
-                            field: 'state', //checobox per fare tutto
-                            checkbox: true,
-                            rowspan: 2,
-                            align: 'center',
-                            valign: 'middle'
-                        }, {
-                            title: 'User ID',
-                            field: 'iduser',
-                            rowspan: 2,
-                            align: 'center',
-                            valign: 'middle',
-                            sortable: true,
-
-                        }, {
-                            title: 'User Detail',
-                            colspan: 6,
-                            align: 'center'
-                        }],
-                        [{
-                                field: 'name',
-                                title: 'User Name',
-                                sortable: true,
-
-                                align: 'center'
-                            }, {
-                                field: 'surname',
-                                title: 'User Surname',
-                                sortable: true,
-                                align: 'center',
-
-                            },
-                            {
-                                field: 'email',
-                                title: 'User Email',
-                                sortable: true,
-                                align: 'center',
-
-                            },
-                            {
-                                field: 'newsletter',
-                                title: 'User Newsletter',
-
-                                align: 'center',
-                                valign: 'middle'
-                            },
-                            {
-                                field: 'category',
-                                title: 'User Category',
-                                sortable: true,
-                                align: 'center',
-
-                            }, {
-                                field: 'operate',
-                                title: 'User Operate',
-                                align: 'center',
-                                clickToSelect: false,
-                                events: window.operateEvents,
-
-                            }
-                        ]
-                    ]
-                })
-                $table.on('check.bs.table uncheck.bs.table ' +
-                    'check-all.bs.table uncheck-all.bs.table',
-                    function() {
-                        $remove.prop('disabled', !$table.bootstrapTable('getSelections').length)
-
-                        // save your data, here just save the current page
-                        selections = getIdSelections()
-                        // push or splice the selections if you want to save all data selections
-                    })
-                $table.on('all.bs.table', function(e, name, args) {
-                    console.log(name, args)
-                })
-                $remove.click(function() {
-                    var ids = getIdSelections()
-                    $table.bootstrapTable('remove', {
-                        field: 'id',
-                        values: ids
-                    })
-                    $remove.prop('disabled', true)
-                })
-            }
-
-            $(function() {
-                initTable()
-
-                $('#locale').change(initTable)
-            })
-        </script>
     </div>
-
-
-
-
-
-
     <?php require_once("footer.php"); ?>
 
 
-
-    <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.js"></script>
 </body>
 
 </html>
+<script> 
+$(document).ready(function() {
+
+$('#users_data').DataTable({
+    paging: true,
+    lengthChange: true,
+    colReorder: true,
+    responsive: true,
+    keys: true,
+    fixedHeader: true,
+    dom: 'lf<"floatright"B>rtip',
+    buttons: [
+        'csv','copy', 'excel', 'pdf', 'print'
+    ] 
+});
+});
+</script>
