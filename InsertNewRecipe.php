abb21cd8af6ed9a9507dcba69a47734a0e0afd8a
Michael Sarto <michael.sarto@ptpvenezia.edu.it>
2021-05-15 13:27:54 +0200

    ihuy

--- InsertNewRecipe.php
+++ InsertNewRecipe.php
@@ -4,8 +4,8 @@
 <head>
     <?php
     require_once("header.php");
-    if(isset($_SESSION['modifyByAdmin']) && isset($_SESSION['modifyByAdmin'])==true){
-        $_SESSION['modifyByAdmin']=false;
+    if (isset($_SESSION['modifyByAdmin']) && isset($_SESSION['modifyByAdmin']) == true) {
+        $_SESSION['modifyByAdmin'] = false;
     }
     if (!isset($_SESSION['Category']) || $_SESSION['Category'] != 'Chef') {
         header("Location: index.php");
@@ -24,22 +24,134 @@
         require_once("callingLogin.php");
         require_once("navbar.php"); ?>
 
-      
-
-
-        
-    <div class="row">
-    
-    
-    
-    </div>
 
 
 
 
+        <header class="text-center logo">
+            <h1 style="font-size: 65px"> Insert the new Recipe </h1>
+        </header>
+
+
+        <form action="InsertNewRecipe.php" method="post" enctype="multipart/form-data">
+            <div class="row d-flex justify-content-center">
+                <div class="col-10  d-flex justify-content-center ">
+
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> Title: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <input type="text" name="title2" maxlength="255" placeholder="Insert the title" style="height: 100%; width:100%; border:none;" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-10  d-flex justify-content-center ">
+
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> Subtitle: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <input type="text" name="subtitle2" maxlength="255" placeholder="Insert the subtitle" style="height: 100%; width:100%; border:none;" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row d-flex justify-content-center">
+                <div class="col-10  d-flex justify-content-center ">
+
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> Time: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <input type="text" name="time2" maxlength="255" placeholder="Insert the PREP | COOKING | REST | TOTAL | SERVES" style="height: 100%; width:100%; border:none;" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row d-flex justify-content-center">
+                <div class="col-10  d-flex justify-content-center ">
+
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> Preparation: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <textarea name="preparation2" maxlength="65535" rows="6" style="height: 100%; width:100%; border:none;" placeholder="Enter the preparation by separating each step by a; and remember to write the number (2 digits) before each step." required></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-10  d-flex justify-content-center ">
+
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> Link: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <input type="text" name="link2" maxlength="255" placeholder="Insert the link" style="height: 100%; width:100%; border:none;" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-10  d-flex justify-content-center ">
+
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> Image: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <input type="file" name="fileToUpload" id="fileToUpload" class="hidden nav-font" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
 
+
+            <div class="row d-flex ">
+                <div class="col-6  d-flex justify-content-center ">
+                    <div class="myform form" style="border: none;">
+                        <input type="submit" class="btn btn-block mybtn btn-secondary logo" name="submit" value="INSERT">
+                    </div>
+                </div>
+                <div class="col-6  d-flex justify-content-center ">
+                    <div class="myform form" style="border: none;">
+                        <input type="reset" class="btn btn-block mybtn btn-secondary logo" name="reset" value="RESET">
+                    </div>
+                </div>
+            </div>
+
+        </form>
+
+        <div class="row d-flex justify-content-center ">
+            <div class="col-12  d-flex justify-content-center ">
+                <div class="myform form" style="border: none;">
+                    <?php
+                    $url = '#';
+                    $action = '';
+                    if ($_SESSION['Category'] != "User") {
+                        switch ($_SESSION['Category']) {
+                            case "Chef":
+                                $url = "chefPageRecipes.php";
+                                $action = "MANAGE RECIPES";
+                                echo  '<a href="chefPageProducts.php" class="button btn btn-block mybtn btn-secondary logo">MANAGE PRODUCTS</a>';
+                                break;
+                            case "Administrator":
+                                $url = 'adminPage.php';
+                                $action = "MANAGE USERS";
+                                break;
+                        }
+                        echo  '<a href="' . $url . '" class="button btn btn-block mybtn btn-secondary logo">' . $action . '</a>';
+                    }
+
+                    ?>
+
+
+
+
+
+                </div>
+            </div>
+        </div>
     </div>
     <?php require_once("footer.php"); ?>
 
