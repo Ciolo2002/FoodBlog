95122512fb6e8f6aed8458a3d6f9bc760d4aaa26
Michael Sarto <michael.sarto@ptpvenezia.edu.it>
2021-05-17 14:17:07 +0200

    ciao

--- InsertNewRecipe.php
+++ InsertNewRecipe.php
@@ -12,7 +12,13 @@
     }
     ?>
 
-
+    <script>
+        $(document).ready(function() {
+            $("#addIngredient").click(function() {
+                $("#ingredient").clone().insertAfter("#preparation");
+            });
+        });
+    </script>
 
 </head>
 
@@ -31,7 +37,17 @@
         <header class="text-center logo">
             <h1 style="font-size: 65px"> Insert the new Recipe </h1>
         </header>
+        <?php
+        $stmt = $dbh->getInstance()->prepare("SELECT * FROM `ingredients` WHERE 1");
+        $stmt->execute();
+        $row;
+        echo '<datalist id="ingredients">';
+        while($row=$stmt->fetch()){
+            echo '<option value="'.$row['Ingredient'].'">';
+        }
+        echo '</datalist>';
 
+        ?>
 
         <form action="InsertNewRecipe.php" method="post" enctype="multipart/form-data">
             <div class="row d-flex justify-content-center">
@@ -70,28 +86,77 @@
                 </div>
             </div>
 
-            <div class="row d-flex justify-content-center">
+            <div class="row d-flex justify-content-center" id="preparation">
                 <div class="col-10  d-flex justify-content-center ">
 
                     <div class="myform form" style="border: none;">
                         <span class="navbar-font" style="font-size: 30px;"> Preparation: </span>
                         <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
-                            <textarea name="preparation2" maxlength="65535" rows="6" style="height: 100%; width:100%; border:none;" placeholder="Enter the preparation by separating each step by a; and remember to write the number (2 digits) before each step." required></textarea>
+                            <textarea name="preparation2" maxlength="65535" rows="6" style="height: 100%; width:100%; border:none;" placeholder="Enter the preparation by separating each step by a ; and remember to write the number (2 digits) before each step." required></textarea>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="row d-flex justify-content-center" id="ingredient">
+                <div class="col-10  d-flex justify-content-center ">
+
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> Ingredient: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <input type="text" list="ingredients" name="ingredient[]" maxlength="255" placeholder="Insert an ingredient" style="height: 100%; width:100%; border:none;" required>
+                            
+
+                        </div>
+                    </div>
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> Quantity: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <input type="text" name="quantity[]" maxlength="255" placeholder="Insert an ingredient" style="height: 100%; width:100%; border:none;" required>
+                        </div>
+                    </div>
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> Measure unit: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <input type="text" name="measureUnit[]" maxlength="255" placeholder="Insert an ingredient" style="height: 100%; width:100%; border:none;" required>
+                        </div>
+                    </div>
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> Alternative: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <input type="text" name="alternative[]" maxlength="255" placeholder="Insert an ingredient" style="height: 100%; width:100%; border:none;">
+                        </div>
+                    </div>
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> A. quantity: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <input type="text" name="measureUnit[]" maxlength="255" placeholder="Insert an ingredient" style="height: 100%; width:100%; border:none;">
+                        </div>
+                    </div>
+                    <div class="myform form" style="border: none;">
+                        <span class="navbar-font" style="font-size: 30px;"> A. m. unit: </span>
+                        <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
+                            <input type="text" name="measureUnit[]" maxlength="255" placeholder="Insert an ingredient" style="height: 100%; width:100%; border:none;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
             <div class="row d-flex justify-content-center">
                 <div class="col-10  d-flex justify-content-center ">
 
                     <div class="myform form" style="border: none;">
-                        <span class="navbar-font" style="font-size: 30px;"> Link: </span>
+                        <span class="navbar-font" style="font-size: 30px;"> Add an ingredient: </span>
                         <div class="form-control navbar-font" style="font-size: 25px; height: auto; width:auto">
-                            <input type="text" name="link2" maxlength="255" placeholder="Insert the link" style="height: 100%; width:100%; border:none;" required>
+                            <button id="addIngredient" style="height: 100%; width:100%; border:none;" required>NEW INGREDIENT</button>
                         </div>
                     </div>
                 </div>
             </div>
+
+
             <div class="row d-flex justify-content-center">
                 <div class="col-10  d-flex justify-content-center ">
 
