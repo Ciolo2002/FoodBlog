7e666c6aa636e7f6935f0aa3814a39601d662e8f
Michael Sarto <michael.sarto@ptpvenezia.edu.it>
2021-05-31 22:24:47 +0200

    fine forse

--- reviews.js
+++ reviews.js
@@ -1,42 +1,42 @@
 $(function () {
-    $("#rateYo").rateYo({ //uso l'id per il valore da inserire a db
+    $("#rateYo").rateYo({ 
         fullStar: true,
         spacing: "5px",
         rating: 4,
         score: 4,
         multiColor: {
 
-            "startColor": "#444444", //GRAY
-            "endColor": "#4d0000" //RED
+            "startColor": "#444444", 
+            "endColor": "#4d0000"
         },
         onSet: function (rating, rateYoInstance) {
-            document.getElementById('hiddenRating').value=rating; //add rating value to input field
+            document.getElementById('hiddenRating').value=rating; 
         },
         onChange: function (rating, rateYoInstance) {
-          document.getElementById('hiddenRating').value=rating; //add rating value to input field
+          document.getElementById('hiddenRating').value=rating; 
           }
     });
    
-    $(".rateyo").rateYo({ //uso la classe perchè questi sono utilizzati come output delle recessioni già avvenute
+    $(".rateyo").rateYo({
         readOnly: true,
         spacing: "5px",
         multiColor: {
 
-            "startColor": "#444444", //GRAY
-            "endColor": "#4d0000" //RED
+            "startColor": "#444444", 
+            "endColor": "#4d0000"
         }
     });
 });
 
-// Getter
-var normalFill = $("#rateYo").rateYo("option", "fullStar"); //returns true
+
+var normalFill = $("#rateYo").rateYo("option", "fullStar"); 
 var ratedFill = $("#rateYo").rateYo("option", "multiColor");
 var readOnly = $(".rateyo").rateYo("option", "readOnly");
 var onSet = $("#rateYo").rateYo("option", "onSet");
 var onChange = $("#rateYo").rateYo("option", "onChange");
 
-// Setter
-$("#rateYo").rateYo("option", "fullStar", true); //returns a jQuery Element
+
+$("#rateYo").rateYo("option", "fullStar", true);
 $("#rateYo").rateYo("option", "multiColor", false);
 $(".rateYo").rateYo("option", "readOnly", false);
 $("#rateYo").rateYo("option", "onSet", function () {
